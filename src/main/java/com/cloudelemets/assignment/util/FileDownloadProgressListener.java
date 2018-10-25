59804dea3807f23b4dbe77d795fe4f566eca0bc7
Eshwar chettri <eshwarchettri02.com>
2018-10-25 11:41:22 +0530

    initial commit

---
+++ src/main/java/com/cloudelemets/assignment/util/FileDownloadProgressListener.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cloudelemets.assignment.util;
+
+/**
+ *
+ * @author Eesha chettri
+ */
+import com.google.api.client.googleapis.media.MediaHttpDownloader;
+import com.google.api.client.googleapis.media.MediaHttpDownloaderProgressListener;
+
+public class FileDownloadProgressListener implements MediaHttpDownloaderProgressListener {
+
+	  @Override
+	  public void progressChanged(MediaHttpDownloader downloader) {
+	    switch (downloader.getDownloadState()) {
+	      case MEDIA_IN_PROGRESS:
+	        View.downloadStatus("Download is in progress: " + downloader.getProgress());
+	        break;
+	      case MEDIA_COMPLETE:
+	        View.downloadStatus("Download is Complete!");
+	        break;
+	    }
+	  }
+	}
+
